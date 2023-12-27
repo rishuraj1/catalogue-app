65e311bdef2787437c7121414e037af3521fd382
Rishu <rrishu561@gmail.com>
2023-12-27 19:57:49 +0530

    feat: configured clerk

---
+++ src/conf.ts
@@ -0,0 +1,3 @@
+export const conf = {
+  clerkPublishableKey: String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY),
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+import { conf } from './conf.ts'
+// console.log(conf.clerkPublishableKey)
+
+if (!conf.clerkPublishableKey) throw new Error('Missing Clerk Publishable Key')
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <div>Home</div>
+      },
+      {
+        path: '/login',
+        element: <div>Login</div>
+      },
+      {
+        path: '/register',
+        element: <div>Register</div>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={conf.clerkPublishableKey}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>,
 )
